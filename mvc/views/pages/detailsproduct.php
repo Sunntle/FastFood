bc5c2f671ddf1798689da903fd2b9c8597b44022
PhamHanHi <huyphps20855@fpt.edu.vn>
2022-11-24 13:57:06 +0700

    change

--- mvc/views/pages/detailsproduct.php
+++ mvc/views/pages/detailsproduct.php
@@ -74,26 +74,22 @@
                     <?php }?>                 
                 </form>
         <?php }else { ?>
-            <button class="btn-login border-0 rounded-3 bg-danger text-white" href='/live/login'> Đăng nhập để bình luận</button>
+            <button class="btn-login border-0 rounded-3 bg-danger text-white px-3 p-1" href='/live/login'> Đăng nhập để bình luận</button>
             <?php } ?>
 <?php } ?>
     <?php foreach($data['CmtID'] as $kq) {?>
-        <div class="items my-2">
-            <div class="nguoi">
-                <div class="text">
-                    <img class="anh" src=" <?php foreach($data['KhachHang'] as $key){ if($kq['maKH'] == $key['maKH']) echo $key['hinh']; }?>" alt="">
-                    <div>
-                        <p><?php foreach($data['KhachHang'] as $key){ if($kq['maKH'] == $key['maKH']) echo $key['tenKH']; }?></p>
-                        <span><?php echo $kq['noiDung']?>!</span>
-                        <p><?php echo $kq['ngayBL']?></p>
-                    </div>
+        <div class="post ">
+            <li class="d-flex gap-3 mb-2 mt-3">
+                <div class="images_cmt">
+                    <img src="<?php foreach($data['KhachHang'] as $key){ if($kq['maKH'] == $key['maKH']) echo $key['hinh']; }?>" alt="">
                 </div>
-                <div class="text1">
-                    
+                <div class="nd_cmt">
+                    <h5 class="name_cmt"><?php foreach($data['KhachHang'] as $key){ if($kq['maKH'] == $key['maKH']) echo $key['tenKH']; }?></h5>
+                    <p class="cmt"><?php echo $kq['noiDung']?></p>
+                    <p class="time_cmt"><?php echo $kq['ngayBL']?></p>
                 </div>
-
-            </div>
-        </div>
+            </li>        
+    </div>
     <?php } ?>
     </div>
     <?php require_once "relatedproducts.php"; ?>
