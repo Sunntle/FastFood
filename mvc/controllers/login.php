098423421fb6adc2be2fd1b3a02c6e74a12af2a5
Tai Le Cong Thanh <taikute012532@gmail.com>
2022-11-27 11:11:36 +0700

    Delete Pass Mail Private

--- mvc/controllers/login.php
+++ mvc/controllers/login.php
@@ -212,7 +212,7 @@ class login extends controller{
                             $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
                             $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
                             $mail->Username   = 'taile2608@gmail.com';                     //SMTP username
-                            $mail->Password   = 'tqtpdwaguyysknqv';                               //SMTP password
+                            $mail->Password   = '';                               //SMTP password
                             $mail->SMTPSecure = 'ssl';            //Enable implicit TLS encryption
                             $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
