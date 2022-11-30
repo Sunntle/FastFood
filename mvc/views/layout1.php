c9335a2b48f741a020929ced112f9a7ac37442d7
le gia huy <chode23con@gmail.com>
2022-11-30 07:40:21 +0700

    iocn

--- mvc/views/layout1.php
+++ mvc/views/layout1.php
@@ -5,12 +5,13 @@
     <meta charset="utf-8">
     <title>Trang quản trị - Three Guys Fast Food</title>
     <base href="http://localhost/live/admin/home">
+    
     <link rel="stylesheet" href="../public/css/styleadmin.css">
     <script src="https://code.jquery.com/jquery-3.4.1.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-    
+    <link rel="icon" type="image/x-icon" href="../public/images/ThreeGuysFastFood.png">
 </head>
 
 <body>
